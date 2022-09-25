c58f906e26850c902bff3f44a3b39e60eb7aab92
Mikaleb <mikaleb@live.fr>
2022-09-25 15:50:54 +0200

    feat(view): init view

---
+++ src/composables/useSpellCheck.ts
@@ -0,0 +1,42 @@
+// import { ref } from 'vue'
+import axios, { AxiosInstance, type AxiosResponse } from 'axios'
+// import { useFontStore } from '../stores/font'
+
+export default function useSpellCheck(contentToCheck: string) {
+  const callApi = () => {
+    const options = {
+      method: 'POST',
+      url: 'https://jspell-checker.p.rapidapi.com/check',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': import.meta.env.VITE_API_PRIVATE_KEY,
+        'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com',
+      },
+      data: `{
+        "language":"enUS",
+        "fieldvalues":"${contentToCheck}",
+        "config":{
+            "forceUpperCase":false,
+            "ignoreIrregularCaps":false,
+            "ignoreFirstCaps":true,
+            "ignoreNumbers":true,
+            "ignoreUpper":false,
+            "ignoreDouble":false,
+            "ignoreWordsWithNumbers":true
+        }}`,
+    }
+
+    return axios
+      .request(options)
+      .then(function (response: AxiosResponse) {
+        return response.data
+      })
+      .catch(function (error: Error) {
+        console.error(error)
+      })
+  }
+
+  return {
+    callApi,
+  }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from 'vue'
+
+import SpellCheck from '../components/SpellCheck.vue'
+import useSpellCheck from '../composables/useSpellCheck'
+
+const contentToCheck = ref('')
+const checkSpelling = () => {
+  const { callApi } = useSpellCheck(contentToCheck.value)
+  callApi().then((res: any) => {
+    console.log(res)
+  })
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="">
+    <SpellCheck v-model="contentToCheck" />
+    <button @click="checkSpelling">Check Spelling</button>
   </main>
 </template>
